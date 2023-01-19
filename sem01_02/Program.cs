9642c39c1d9a2ccd484b18009c3caa70bea02808
Tatyana <grigtat13@gmail.com>
2023-01-19 12:12:23 +0300

    sem01_02

---
+++ sem01_02/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите первое число:");
+string numberStr1 = Console.ReadLine();
+int a = Convert.ToInt32(numberStr1);
+
+/*Принять два числа, первое является квадратом второго*/
+
+Console.WriteLine("Введите второе число:");
+string numberStr2 = Console.ReadLine();
+int b = Convert.ToInt32(numberStr2);
+
+int c = a/b;
+
+if (c == b)
+{
+Console.WriteLine("Число является квадратом ");
+}
+else 
+{
+Console.WriteLine("Число не является квадратом");
+}
