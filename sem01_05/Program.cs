0fea5ac6c903b4db00144b55795fe4c8d6837148
Tatyana <grigtat13@gmail.com>
2023-01-19 13:32:47 +0300

    sem01_05

---
+++ sem01_05/Program.cs
@@ -0,0 +1,9 @@
+﻿/* Напишите программу, которая приниамет на вход 
+трехзначное число и на выходе показывает последнюю 
+цифру этого числа */ 
+
+Console.WriteLine("Напишите трехзначное число");
+string number = Console.ReadLine();
+int a = Convert.ToInt32(number);
+int b = a % 10;
+Console.WriteLine(b);
