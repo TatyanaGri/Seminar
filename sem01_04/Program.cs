07b8a0c15555a3ff5d322fb453886f30606c82d4
Tatyana <grigtat13@gmail.com>
2023-01-19 13:19:54 +0300

    sem01_04

---
+++ sem01_04/Program.cs
@@ -0,0 +1,12 @@
+﻿/* Принимает число N, а на выходе показывает все целые числа
+4 -> -4, -3, -2, -1, 0, 1, 2, 3, 4
+*/
+
+Console.WriteLine("Напишите число");
+string numberStr = Console.ReadLine();
+int number = Convert.ToInt32(numberStr);
+
+for (int i = - number; i <= number; i++)
+{
+    Console.Write(i + " ");
+}
