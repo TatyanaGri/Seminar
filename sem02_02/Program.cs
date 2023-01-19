7c226018453c34c87eb491b748a730611ca568d1
Tatyana <grigtat13@gmail.com>
2023-01-19 14:46:54 +0300

    sem02_01

---
+++ sem02_02/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Программа которая вывод случайное трехзначное
+ число и удаляет вторую цифру этого числа
+ 456 -> 46 
+  */
+
+int GetRandomValue()
+{
+    return new Random().Next(100, 1000);
+}
+
+int DeletSecondNumber(int number)
+{
+    return (number / 100) * 10 + (number % 10);
+}
+
+int number = GetRandomValue();
+int result = DeletSecondNumber(number);
+Console.WriteLine($"Было {number}, стало {result}");
