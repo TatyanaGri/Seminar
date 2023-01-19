131444da07b1c009abd783dfeeff504da17d6bdf
Tatyana <grigtat13@gmail.com>
2023-01-19 11:59:24 +0300

    sem01_01

--- sem01_01/Program.cs
+++ sem01_01/Program.cs
@@ -1,4 +1,7 @@
-﻿Console.WriteLine("Введите число:");
+﻿/* Напишите программу которая на вход принимает число и выдает его умножение на самого себя */
+
+
+Console.WriteLine("Введите число:");
 string numberStr = Console.ReadLine();
 int number = Convert.ToInt32(numberStr);
 
