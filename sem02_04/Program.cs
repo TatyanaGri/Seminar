5ac231850a6379c1af884cc8a9f2a91d61fc0de2
Tatyana <grigtat13@gmail.com>
2023-01-19 17:15:56 +0300

    sem02_04

---
+++ sem02_04/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Получить на вдох число и проверить кратно оно 7 и 23 одновременно */
+
+
+int GetNumber()
+{
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+Console.WriteLine("Напишите число");
+int Number = GetNumber();
+
+if (Number % 7 == 0 && Number % 23 == 0)
+{
+    Console.WriteLine($"Число {Number} кратно 7 и 23");
+}
+else
+{
+    Console.WriteLine($"Число {Number} не кратно 7 и 23");
+}
