b17090271e63cc4655daf75e17cf21726806a563
Tatyana <grigtat13@gmail.com>
2023-01-23 11:08:33 +0300

    sem

---
+++ sem01/sem01_01/Program.cs
@@ -0,0 +1,10 @@
+﻿/* Напишите программу которая на вход принимает число и выдает его умножение на самого себя */
+
+
+Console.WriteLine("Введите число:");
+string numberStr = Console.ReadLine();
+int number = Convert.ToInt32(numberStr);
+
+int result = number * number;
+
+Console.WriteLine($"Квадрат числа {number} = {result}");
